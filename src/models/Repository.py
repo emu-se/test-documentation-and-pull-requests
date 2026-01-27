cf075acfe048410add91ddadd1663587099cd231
Teal <github@tealamore.com>
2026-01-27 18:13:13 -0500

    Delete src/models/Repository.py

--- src/models/Repository.py
+++
@@ -1,8 +0,0 @@
-from typing import Any
-
-class Repository:
-    def __init__(self, data: dict[str, Any]):
-        self.full_name = data.get("full_name")
-        self.stargazers_count = data.get("stargazers_count")
-        self.forks_count = data.get("forks_count")
-        self.watchers_count = data.get("watchers_count")
